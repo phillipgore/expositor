9b0e8e1df7b8e29685ff8fc510bff9c8469d6643
Phillip Gore <phillipagore@me.com>
2025-10-04 15:37:34 -0500

    reverted home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,29 +2,20 @@
 	import { isAuthenticated } from '$lib/stores/auth.js';
 	import ToolbarAuth from '$lib/components/ToolbarAuth.svelte';
 	import Icon from '$lib/elements/Icon.svelte';
+	import Heading from '$lib/elements/Heading.svelte';
 </script>
 
 <ToolbarAuth></ToolbarAuth>
 
-<div class="wrapper">
+<div class="branding-container">
 	<div class="branding">
 		<Icon iconId="book" isActive={false} classes=""></Icon>
-		<h1>Expositor App</h1>
-		
-		{#if $isAuthenticated}
-			<p>Welcome back! <a href="/new">Continue to your work</a></p>
-		{:else}
-			<p>A powerful tool for biblical exposition and study.</p>
-			<div class="auth-links">
-				<a href="/signin" class="auth-link">Sign In</a>
-				<a href="/signup" class="auth-link">Sign Up</a>
-			</div>
-		{/if}
+		<Heading heading="h1" alignCenter>Expositor App</Heading>
 	</div>
 </div>
 
 <style>
-	.wrapper {
+	.branding-container {
 		display: flex;
 		flex-direction: column;
 		flex-grow: 1;
@@ -35,59 +26,7 @@
 			display: flex;
 			flex-direction: column;
 			align-items: center;
-			margin-bottom: 12rem;
-
-			h1 {
-				font-size: 4rem;
-				margin: 0 0 0.5em;
-				color: var(--black);
-				text-align: center;
-			}
-
-			p {
-				color: var(--gray-400);
-				font-size: 1.4rem;
-				text-align: center;
-				margin-bottom: 2rem;
-				
-				a {
-					color: var(--system-blue);
-					text-decoration: none;
-					
-					&:hover {
-						text-decoration: underline;
-					}
-				}
-			}
-
-			.auth-links {
-				display: flex;
-				gap: 1.5rem;
-				margin-top: 1rem;
-			}
-
-			.auth-link {
-				background-color: var(--system-blue);
-				color: white;
-				text-decoration: none;
-				padding: 0.8rem 1.6rem;
-				border-radius: 0.3rem;
-				font-size: 1.2rem;
-				font-weight: 500;
-				transition: background-color 0.2s;
-
-				&:hover {
-					background-color: #0056b3;
-				}
-
-				&:first-child {
-					background-color: var(--system-gray);
-					
-					&:hover {
-						background-color: #5a5a5a;
-					}
-				}
-			}
+			padding-bottom: 9.0rem;
 
 			:global(.icon) {
 				height: 6rem;
